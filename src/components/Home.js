869e297f156758434bd65a6aa2917805be315b62
ankm19 <ankm335@gmail.com>
2022-08-23 16:53:10 +0530

    collecting the correct data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
 
 const Home = () => {
+  // calling the endpoint
 
-    // calling the endpoint
-    const data = async (ext) => {
-        const response = await fetch("http://localhost:5000/module/getdbdata", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ symbol: ext }),
-        });
-        return response;
-    }
-    console.log(data);
-  return (
-    <div className='container'>
-      Home Graph
-    </div>
-  )
-}
+  const data = async () => {
+    console.log("hellog");
+    const response = await fetch("http://localhost:5000/module/getdbdata", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ simnumber: "1_EVD" }),
+    });
+    const json = await response.json();
+    let d = [];
+    d.push(json);
+    console.log(d);
+  };
 
-export default Home
+  useEffect(()=>{
+    data();
+  },[])
+  return <div className="container">Home Graph</div>;
+};
+
+export default Home;
