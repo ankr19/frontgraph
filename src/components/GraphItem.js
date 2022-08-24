3cb60a1629e10c3abe4b0424fe0c7bd514f93dcb
ankm19 <ankm335@gmail.com>
2022-08-24 11:43:03 +0530

    change

--- src/components/GraphItem.js
+++ src/components/GraphItem.js
@@ -2,22 +2,34 @@ import React from "react";
 import Plot from "react-plotly.js";
 const GraphItem = (props) => {
   const { value, date } = props;
-  
-  let d = new Array((value[0]).length).fill(date);
- 
- 
+
+  let d = new Array(value[0].length).fill(date);
+
   return (
     <div>
       <Plot
-        data={[{
-          x: value[0],
-          y: d,
-          type: 'scatter',
-          mode: 'lines+markers',
-          marker: {color: 'red'},
-        },]}
+        data={[
+          {
+            x: value[0],
+            y: d,
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "red" },
+          },
+          {
+            x: value[1],
+            y: d,
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "green" },
+          },
+        ]}
         layout={{ width: 800, height: 500, title: "A Fancy Plot" }}
       />
+      {/* <Plot
+        data={[,]}
+        layout={{ width: 800, height: 500, title: "A Fancy Plot" }}
+      /> */}
     </div>
   );
 };
