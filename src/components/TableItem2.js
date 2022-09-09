1f72a188034acc479f7cb84eedb20ef500a6c42d
ankm19 <ankm335@gmail.com>
2022-09-09 13:04:21 +0530

    changesintable

--- src/components/TableItem2.js
+++ src/components/TableItem2.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import React, { useContext } from 'react'
+import GraphContext from '../context/GraphContext'
 
 const TableItem2 = () => {
+    const context = useContext(GraphContext);
+    const {data} = context;
+
+    const columns = [
+        { field: 'Date', headerName: 'Date', width: 70 },]
   return (
-    <div>
-      
+    <div style={{height: 400, width: '100%'}}>
+      <DataGrid
+      rows={data}
+      columns={columns}
+      pageSize={10}
+        rowsPerPageOptions={[10]}
+        checkboxSelection
+      />
     </div>
   )
 }
--- src/components/TableJ.js
+++ src/components/TableJ.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import GraphContext from "../context/GraphContext";
 import TableItem from "./TableItem";
+import TableItem2 from "./TableItem2";
 import './tablej.css'
 const TableJ = () => {
   const context = useContext(GraphContext);
@@ -13,11 +14,12 @@ const TableJ = () => {
   return (
     <div>
       <Stack spacing={2} direction="row">
-        <Button style={{fontSize:"15px",border:"2px solid blue"}}>
+        <Button style={{fontSize:"15px",border:"2px solid blue", margin:"5px"}}>
           <Link to="/" style={{color:"blue"}}>Home</Link>
         </Button>
       </Stack>
-      <TableItem/>
+      {/* <TableItem/> */}
+      <TableItem2/>
     </div>
   );
 };
