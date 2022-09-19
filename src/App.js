1efb27cf5d65b23f5d3e019d4ddf39a4134f4ba2
ankm19 <ankm335@gmail.com>
2022-09-19 12:31:50 +0530

    changing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import TableJ from "./components/TableJ";
 import Graph from "./components/Graph";
+import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
   return (
     <GraphState>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home2 />} />
-          <Route path="table" element={<TableJ />} />
-          <Route path="graph" element={<Graph />} />
-        </Routes>
+      <div className="container-fuild">
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Home2 />} />
+            <Route path="/table" element={<TableJ />} />
+            <Route path="/graph" element={<Graph />} />
+          </Routes>
+        </Sidebar>
       </div>
     </GraphState>
   );
--- src/components/Home2.js
+++ src/components/Home2.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Button, Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar/Sidebar";
 const Home2 = () => {
   return (
     <div>
-      <Stack spacing={2} direction="row">
+      {/* <Outlet/> */}
+      {/* <Stack spacing={2} direction="row">
         <Button style={{ fontSize:"150px", border:"2px solid blue"}}>
           <Link to="table" style={{color:"blue"}}>Table</Link>
         </Button>
         <Button style={{fontSize:"150px",border:"2px solid blue"}}>
           <Link to="graph" style={{color:"blue"}}>Graph</Link>
         </Button>
-      </Stack>
+      </Stack> */}
     </div>
   );
 };
